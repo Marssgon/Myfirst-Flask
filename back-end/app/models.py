ad62b4e5afccabfb7f9de67745cb6faea81f2eb5
Marssgon <982076553@qq.com>
2021-02-22 17:58:20 +0800

    11. 私信

--- back-end/app/models.py
+++ back-end/app/models.py
@@ -89,6 +89,17 @@ class User(PaginatedAPIMixin, db.Model):
     # 用户的通知
     notifications = db.relationship('Notification', backref='user',
                                     lazy='dynamic', cascade='all, delete-orphan')
+    # 用户发送的私信
+    messages_sent = db.relationship('Message', foreign_keys='Message.sender_id',
+                                    backref='sender', lazy='dynamic',
+                                    cascade='all, delete-orphan')
+    # 用户接收的私信
+    messages_received = db.relationship('Message',
+                                        foreign_keys='Message.recipient_id',
+                                        backref='recipient', lazy='dynamic',
+                                        cascade='all, delete-orphan')
+    # 用户最后一次查看私信的时间
+    last_messages_read_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -207,14 +218,26 @@ class User(PaginatedAPIMixin, db.Model):
         return n
 
     def new_recived_comments(self):
-        '''用户发布的文章下面收到的新评论计数'''
+        '''用户收到的新评论计数
+                包括:
+                1. 用户的所有文章下面新增的评论
+                2. 用户发表的评论(或下面的子孙)被人回复了
+                '''
         last_read_time = self.last_recived_comments_read_time or datetime(1900, 1, 1)
         # 用户发布的所有文章
         user_posts_ids = [post.id for post in self.posts.all()]
-        # 用户收到的所有评论，即评论的 post_id 在 user_posts_ids 集合中，且评论的 author 不是当前用户（即文章的作者）
-        recived_comments = Comment.query.filter(Comment.post_id.in_(user_posts_ids), Comment.author != self).order_by(Comment.mark_read, Comment.timestamp.desc())
-        # 新评论
-        return recived_comments.filter(Comment.timestamp > last_read_time).count()
+        # 用户文章下面的新评论, 即评论的 post_id 在 user_posts_ids 集合中，且评论的 author 不是自己(文章的作者)
+        q1 = set(Comment.query.filter(Comment.post_id.in_(user_posts_ids), Comment.author != self).all())
+
+        # 用户发表的评论被人回复了，找到每个用户评论的所有子孙
+        q2 = set()
+        for c in self.comments:
+            q2 = q2 | c.get_descendants()
+        q2 = q2 - set(self.comments.all())  # 除去子孙中，用户自己发的(因为是多级评论，用户可能还会在子孙中盖楼)，自己回复的不用通知
+        # 用户收到的总评论集合为 q1 与 q2 的并集
+        recived_comments = q1 | q2
+        # 最后，再过滤掉 last_read_time 之前的评论
+        return len([c for c in recived_comments if c.timestamp > last_read_time])
 
     def new_follows(self):
         '''用户的新粉丝计数'''
@@ -243,6 +266,11 @@ class User(PaginatedAPIMixin, db.Model):
         last_read_time = self.last_followeds_posts_read_time or datetime(1900, 1, 1)
         return self.followeds_posts().filter(Post.timestamp > last_read_time).count()
 
+    def new_recived_messages(self):
+        '''用户未读的私信计数'''
+        last_read_time = self.last_messages_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time).count()
 
 class Post(PaginatedAPIMixin, db.Model):
     __tablename__ = 'posts'
@@ -415,6 +443,37 @@ class Notification(db.Model):  # 不需要分页
             '_links': {
                 'self': url_for('api.get_notification', id=self.id),
                 'user_url': url_for('api.get_user', id=self.user_id)
+            }
+        }
+        return data
+
+    def from_dict(self, data):
+        for field in ['body', 'timestamp']:
+            if field in data:
+                setattr(self, field, data[field])
+
+class Message(PaginatedAPIMixin, db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.id)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'body': self.body,
+            'timestamp': self.timestamp,
+            'sender': self.sender.to_dict(),
+            'recipient': self.recipient.to_dict(),
+            '_links': {
+                'self': url_for('api.get_message', id=self.id),
+                'sender_url': url_for('api.get_user', id=self.sender_id),
+                'recipient_url': url_for('api.get_user', id=self.recipient_id)
             }
         }
         return data
